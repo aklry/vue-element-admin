69517b79c84b425fad462227e27e02536e3da582
Aklry <74409739+aklry@users.noreply.github.com>
2021-11-09 22:20:08 +0800

    fix(form.vue): 修改表单校验功能

--- src/views/test/form.vue
+++ src/views/test/form.vue
@@ -4,16 +4,20 @@
       <div class="content">
         <span>活动名称：</span><el-input v-model="value" placeholder="请输入内容" @blur="validate" />
       </div>
+    </s-validate>
+    <s-validate #default="{ validate }" :rules="rules" :value="zone">
       <div class="content">
-        <span>活动区域：</span><el-input v-model="zone" placeholder="请输入工作区域" />
-      </div>
-      <div class="content">
-        <span>活动时间：</span><el-input v-model="date" placeholder="请选择日期" suffix-icon="el-icon-date" />
-      </div>
-      <div class="content">
-        <el-button type="primary">立即创建</el-button>
+        <span>活动区域：</span><el-input v-model="zone" placeholder="请输入工作区域" @blur="validate" />
       </div>
     </s-validate>
+    <s-validate #default="{ validate }" :rules="dateRules" :value="date">
+      <div class="content">
+        <span>活动时间：</span><el-input v-model="date" placeholder="请选择日期" suffix-icon="el-icon-date" @blur="validate" />
+      </div>
+    </s-validate>
+    <div class="content">
+      <el-button type="primary">立即创建</el-button>
+    </div>
   </div>
 </template>
 
@@ -32,6 +36,14 @@ export default {
         {
           test: function(value) {
             return /^[\u4e00-\u9fa5]/.test(value)
+          },
+          message: '请输入中文'
+        }
+      ],
+      dateRules: [
+        {
+          test: function(value) {
+            return / ^(\d{4})-(\d{2})-(\d{2})$ /.test(value)
           },
           message: '请输入中文'
         }
