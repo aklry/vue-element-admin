7530e5692d8dac48cbc3b17b4d2274bc5a2898b6
Aklry <74409739+aklry@users.noreply.github.com>
2021-11-09 20:47:09 +0800

    feat(src/components/PermissionsButton/index.vue): 添加一个权限更换按钮组件

---
+++ src/components/PermissionsButton/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <el-button icon="el-icon-message-solid" @click="changePermission" />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    changePermission(val) {
+      this.$emit('change', val)
+    }
+  }
+}
+</script>
+
